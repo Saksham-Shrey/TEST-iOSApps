c041116604a697268248eefbfebaac41430d2ec1
Saksham Shrey <sakshamshrey_work@icloud.com>
2024-04-04 11:46:36 +0530

    updated viewControl

--- FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
     
     var messages: [Message] = [
         Message(sender: "a@gmail.com", body: "Hello B"),
@@ -32,6 +35,16 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+                if let e = error {
+                    print("There was an issue in saving data")
+                } else {
+                    print("Successfully saved data")
+                }
+            }
+        }
         
     }
     
